70e102b890515e8125f2a46f3302b5b63d030e3e
a0zhar <96749372+a0zhar@users.noreply.github.com>
2024-04-12 04:20:33 +0200

    Update ScanThread.cs
    Performed Code Reoranization.
    Performed Code Cleanup.

--- PS4_Cheater/ScanThread.cs
+++ PS4_Cheater/ScanThread.cs
@@ -1,80 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Threading;
 
 namespace PS4_Cheater {
-    class PeekThread {
-        private ProcessManager processManager;
-        //private BackgroundWorker worker;
-        private List<byte[]> buffer_queue;
 
+    internal class ComparerThread {
+        private List<byte[]> buffer_queue;
+        private int consumer_idx = 0;
         private Semaphore consumer_mutex;
-        private Semaphore producer_mutex;
-
-        private int productor_idx = 0;
-
-        public PeekThread(ProcessManager processManager, List<byte[]> bufferQueue,
-            Semaphore consumerMutex, Semaphore producerMutex) {
-            this.processManager = processManager;
-            this.buffer_queue = bufferQueue;
-            this.consumer_mutex = consumerMutex;
-            this.producer_mutex = producerMutex;
-            this.productor_idx = 0;
-        }
-
-        public void Peek() {
-            for (int section_idx = 0; section_idx < processManager.MappedSectionList.Count; ++section_idx) {
-                MappedSection mappedSection = processManager.MappedSectionList[section_idx];
-
-                if (!mappedSection.Check) {
-                    mappedSection.ResultList = null;
-                    continue;
-                }
-
-                ulong address = mappedSection.Start;
-                int length = mappedSection.Length;
-
-                while (length != 0) {
-                    int cur_length = CONSTANT.PEEK_BUFFER_LENGTH;
-
-                    if (cur_length > length) {
-                        cur_length = length;
-                        length = 0;
-                    } else {
-                        length -= cur_length;
-                    }
-
-                    //if (worker.CancellationPending) break;
-
-                    producer_mutex.WaitOne();
-                    //buffer_queue[productor_idx] = memoryHelper.ReadMemory(address, (int)cur_length);
-                    productor_idx = (productor_idx + 1) % CONSTANT.MAX_PEEK_QUEUE;
-                    consumer_mutex.Release();
-
-                    address += (ulong)cur_length;
-                }
-            }
-        }
-    }
-
-    class ComparerThread {
-        private ProcessManager processManager;
-
-        private MemoryHelper memoryHelper;
-
-        private List<byte[]> buffer_queue;
-
         private byte[] default_value_0 = null;
         private byte[] default_value_1 = null;
-
-        private BackgroundWorker worker;
-
-        private Semaphore consumer_mutex;
+        private MemoryHelper memoryHelper;
+        private ProcessManager processManager;
         private Semaphore producer_mutex;
+        private BackgroundWorker worker;
         private Mutex worker_mutex;
-
-        private int consumer_idx = 0;
 
         public ComparerThread(ProcessManager processManager, MemoryHelper memoryHelper, List<byte[]> bufferQueue,
             string value_0, string value_1, BackgroundWorker worker, Semaphore consumerMutex, Semaphore producerMutex, Mutex workerMutex) {
@@ -117,7 +58,8 @@ namespace PS4_Cheater {
                     if (cur_length > length) {
                         cur_length = length;
                         length = 0;
-                    } else {
+                    }
+                    else {
                         length -= cur_length;
                     }
 
@@ -184,7 +126,8 @@ namespace PS4_Cheater {
                     if (cur_length > length) {
                         cur_length = length;
                         length = 0;
-                    } else {
+                    }
+                    else {
                         length -= cur_length;
                     }
 
@@ -227,6 +170,62 @@ namespace PS4_Cheater {
                 if (mappedSection.Check)
                     processed_memory_len += mappedSection.Length;
                 worker.ReportProgress((int)(((float)processed_memory_len / total_memory_size) * 80));
+            }
+        }
+    }
+
+    internal class PeekThread {
+
+        //private BackgroundWorker worker;
+        private List<byte[]> buffer_queue;
+
+        private Semaphore consumer_mutex;
+        private ProcessManager processManager;
+        private Semaphore producer_mutex;
+
+        private int productor_idx = 0;
+
+        public PeekThread(ProcessManager processManager, List<byte[]> bufferQueue,
+            Semaphore consumerMutex, Semaphore producerMutex) {
+            this.processManager = processManager;
+            this.buffer_queue = bufferQueue;
+            this.consumer_mutex = consumerMutex;
+            this.producer_mutex = producerMutex;
+            this.productor_idx = 0;
+        }
+
+        public void Peek() {
+            for (int section_idx = 0; section_idx < processManager.MappedSectionList.Count; ++section_idx) {
+                MappedSection mappedSection = processManager.MappedSectionList[section_idx];
+
+                if (!mappedSection.Check) {
+                    mappedSection.ResultList = null;
+                    continue;
+                }
+
+                ulong address = mappedSection.Start;
+                int length = mappedSection.Length;
+
+                while (length != 0) {
+                    int cur_length = CONSTANT.PEEK_BUFFER_LENGTH;
+
+                    if (cur_length > length) {
+                        cur_length = length;
+                        length = 0;
+                    }
+                    else {
+                        length -= cur_length;
+                    }
+
+                    //if (worker.CancellationPending) break;
+
+                    producer_mutex.WaitOne();
+                    //buffer_queue[productor_idx] = memoryHelper.ReadMemory(address, (int)cur_length);
+                    productor_idx = (productor_idx + 1) % CONSTANT.MAX_PEEK_QUEUE;
+                    consumer_mutex.Release();
+
+                    address += (ulong)cur_length;
+                }
             }
         }
     }
